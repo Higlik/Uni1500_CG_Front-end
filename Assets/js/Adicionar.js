0f5053f5be9dbc9bb3a6015e4b82f96621b35a9c
TarsilaNascMartins <tarsilawho@gmail.com>
2022-08-01 15:32:22 -0300

    Duplicar linha

--- Assets/js/Adicionar.js
+++ Assets/js/Adicionar.js
@@ -1,1 +1,24 @@
-// JavaScript source code
+
+document.getElementById("btnCriar").addEventListener("click", addRows);
+
+function addRows() {
+	var table = document.getElementById('tabela');
+	var rowCount = table.rows.length;
+	var cellCount = table.rows[0].cells.length;
+	var row = table.insertRow(rowCount);
+	for (var i = 0; i <= cellCount; i++) {
+		var cell = 'cell' + i;
+		cell = row.insertCell(i);
+		var copycel = document.getElementById('col' + i).innerHTML;
+		cell.innerHTML = copycel;
+		if (i == 3) {
+			var radioinput = document.getElementById('col3').getElementsByTagName('input');
+			for (var j = 0; j <= radioinput.length; j++) {
+				if (radioinput[j].type == 'radio') {
+					var rownum = rowCount;
+					radioinput[j].name = 'gender[' + rownum + ']';
+				}
+			}
+		}
+	}
+}
--- Assets/js/Duplicar.js
+++ Assets/js/Duplicar.js
@@ -1,1 +1,16 @@
 ﻿
+
+
+document.getElementById("btnDuplica").addEventListener("click", addRows);
+
+function addRows() { 
+
+	const node = document.getElementById("coluna");
+	const clone = node.cloneNode(true);
+	document.body.appendChild(clone);
+
+
+	
+	
+}
+
